c6d1fa1adfbcb2821e93517145e9e85d326851a4
iqbalanandra <iqbalcopo70@gmail.com>
2024-06-18 18:57:29 +0700

    Update Database
    adding favorite feature

--- pujang (6).sql
+++ pujang (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2024 at 11:34 AM
+-- Generation Time: Jun 18, 2024 at 01:56 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -45,9 +45,9 @@ CREATE TABLE `buku` (
 INSERT INTO `buku` (`ISBN`, `judul`, `penulis`, `deskripsi`, `src_gambar`, `published`, `language`, `publisher`) VALUES
 ('9780062073488', 'Lalu Semuanya Lenyap', 'Agatha Christie', 'Buku ini bercerita tentang Sepuluh orang diundang ke sebuah rumah mewah dan modern di sebuah pulau di seberang pantai Devon. Sepuluh orang dengan rahasia kelam masing-masing, datang tanpa curiga pada sore musim panas yang indah. Tetapi tiba-tiba terjadi serentetan kejadian misterius. Pulau tersebut berubah menjadi pulau maut yang mengerikan ketika orang-orang itu tewas satu demi satu... Novel Agatha Christie yang paling mencekam dan mendengangkan! Cerita detektif tanpa detektif!', '../image/cover/3.png', '1939', 'Indonesia', 'St. Martin\'s Griffin'),
 ('9780156012195', 'The Little Prince', 'Antoine de Saint-Exupéry', 'The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris, and his release to live in London with his daughter Lucie whom he had never met. The story is set against the conditions that led up to the French Revolution and the Reign of Terror.', '../image/cover/4.png', '1945', 'English', 'Gallimard'),
-('9780307887436', 'Ready Player One', 'Ernest Cline', 'IN THE YEAR 2044, reality is an ugly place. The only time teenage Wade Watts really feels alive is when he\'s jacked into the virtual utopia known as the OASIS. Wade\'s devoted his life to studying the puzzles hidden within this world\'s digital confines, puzzles that are based on their creator\'s obsession with the pop culture of decades past and that promise massive power and fortune to whoever can unlock them.But when Wade stumbles upon the first clue, he finds himself beset by players willing to kill to take this ultimate prize. The race is on, and if Wade\'s going to survive, he\'ll have to win—and confront the real world he\'s always been so desperate to escape.', '../image/cover/6.png', '2011', 'English', 'Crown Publisher'),
+('9780307887436', 'Ready Player One', 'Ernest Cline', 'IN THE YEAR 2044, reality is an ugly place. The only time teenage Wade Watts really feels alive is when he\'s jacked into the virtual utopia known as the OASIS. Wade\'s devoted his life to studying the puzzles hidden within this world\'s digital confines, puzzles that are based on their creator\'s obsession with the pop culture of decades past and that promise massive power and fortune to whoever can unlock them.  But when Wade stumbles upon the first clue, he finds himself beset by players willing to kill to take this ultimate prize. The race is on, and if Wade\'s going to survive, he\'ll have to win—and confront the real world he\'s always been so desperate to escape.', '../image/cover/6.png', '2011', 'English', 'Crown Publisher'),
 ('9780345445605', 'The Hobbit', 'J.R.R Tolkien', 'The Hobbit is set in Middle-earth and follows home-loving Bilbo Baggins, the hobbit of the title, who joins the wizard Gandalf and the thirteen dwarves of Thorin\'s Company, on a quest to reclaim the dwarves\' home and treasure from the dragon Smaug. Bilbo\'s journey takes him from his peaceful rural surroundings into more sinister territory.', '../image/cover/2.png', '1937', 'English', 'George Allen & Unwin'),
-('9780553418026', 'The Martian', 'Andy welt', 'Six days ago, astronaut Mark Watney became one of the first people to walk on Mars.Now, he’s sure he’ll be the first person to die there.After a dust storm nearly kills him and forces his crew to evacuate while thinking him dead, Mark finds himself stranded and completely alone with no way to even signal Earth that he’s alive—and even if he could get word out, his supplies would be gone long before a rescue could arrive.Chances are, though, he won’t have time to starve to death. The damaged machinery, unforgiving environment, or plain-old “human error” are much more likely to kill him first.', '../image/cover/5.png', '2011', 'English', 'Ballantine Books'),
+('9780553418026', 'The Martian', 'Andy welt', 'Six days ago, astronaut Mark Watney became one of the first people to walk on Mars. Now, he’s sure he’ll be the first person to die there. After a dust storm nearly kills him and forces his crew to evacuate while thinking him dead, Mark finds himself stranded and completely alone with no way to even signal Earth that he’s alive—and even if he could get word out, his supplies would be gone long before a rescue could arrive.  Chances are, though, he won’t have time to starve to death. The damaged machinery, unforgiving environment, or plain-old “human error” are much more likely to kill him first.', '../image/cover/5.png', '2011', 'English', 'Ballantine Books'),
 ('9781250076960', 'Six Of Crows', 'Leigh Bardugo', 'Ketterdam: a bustling hub of international trade where anything can be had for the right price—and no one knows that better than criminal prodigy Kaz Brekker. Kaz is offered a chance at a deadly heist that could make him rich beyond his wildest dreams. But he can’t pull it off alone. . . .', '../image/cover/7.png', '2015', 'English', 'Henry Holt'),
 ('9781503219700', 'A Tale of Two Cities', 'Charles Dickens', 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way—in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.', '../image/cover/1.png', '1859', 'English', 'Chapman & Hall'),
 ('9783442178582', 'Atomic Habits', 'James Clear', 'If you\'re having trouble changing your habits, the problem isn\'t you. The problem is your system. Bad habits repeat themselves again and again not because you don\'t want to change, but because you have the wrong system for change. You do not rise to the level of your goals. You fall to the level of your systems. Here, you\'ll get a proven system that can take you to new heights.', '../image/cover/8.png', '2018', 'English', 'Avery');
@@ -114,6 +114,7 @@ CREATE TABLE `users` (
   `nama` varchar(255) NOT NULL,
   `email` varchar(80) NOT NULL,
   `password` varchar(16) NOT NULL,
+  `src_profile` varchar(64) NOT NULL DEFAULT '../image/profile/default.png',
   `level` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -121,14 +122,12 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `nama`, `email`, `password`, `level`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com', '123', 'admin'),
-(2, 'iqbal', 'Muhammad Iqbal Anandra', 'iqbal@gmail.com', '321', 'user'),
-(3, 'a', '', 'a@gmail.com', 'a', 'user'),
-(5, 'ehlen', 'ahlan', 'alen@gmail.com', 'lope', 'user'),
-(7, 'Jambi', 'Rifqi', 'jambi@gmail.com', 'jambialltheway', 'user'),
-(8, 'arip', 'Arip Saputra', 'arip@gmail.com', 'arip', 'user'),
-(9, 'fer', 'bintang fer', 'bintang@gmail.com', '123', 'user');
+INSERT INTO `users` (`id`, `username`, `nama`, `email`, `password`, `src_profile`, `level`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', '123', '../image/profile/default.png', 'admin'),
+(2, 'iqbal', 'Muhammad Iqbal Anandra', 'iqbal@gmail.com', '321', '/Pujang_ADSI/image/profile/iqbal.jpg', 'user'),
+(7, 'Jambi', 'Rifqi', 'jambi@gmail.com', 'jambialltheway', '../image/profile/rifki.jpg', 'user'),
+(8, 'arip', 'Arip Saputra', 'arip@gmail.com', 'arip', '../image/profile/default.png', 'user'),
+(9, 'fer', 'bintang fer', 'bintang@gmail.com', '123', '../image/profile/bintang.png', 'user');
 
 -- --------------------------------------------------------
 
@@ -140,8 +139,27 @@ CREATE TABLE `user_buku` (
   `id_user_buku` int(11) NOT NULL,
   `ISBN` varchar(15) NOT NULL,
   `id` int(11) NOT NULL,
-  `status` varchar(20) NOT NULL
+  `status` varchar(20) NOT NULL,
+  `is_favorite` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_buku`
+--
+
+INSERT INTO `user_buku` (`id_user_buku`, `ISBN`, `id`, `status`, `is_favorite`) VALUES
+(1, '9780156012195', 2, 'Reading', 0),
+(2, '9780062073488', 2, 'Reading', 0),
+(3, '9781250076960', 8, 'On Hold', 0),
+(4, '9780345445605', 8, 'Completed', 0),
+(5, '9780307887436', 8, 'Completed', 0),
+(6, '9780307887436', 2, 'Plan to Read', 0),
+(7, '9781250076960', 2, 'Plan to Read', 0),
+(8, '9781503219700', 2, 'Dropped', 0),
+(9, '9783442178582', 2, 'On Hold', 0),
+(10, '9780553418026', 2, 'Completed', 1),
+(11, '9780345445605', 2, 'Dropped', 0),
+(13, '9780553418026', 8, 'On Hold', 1);
 
 --
 -- Indexes for dumped tables
@@ -194,6 +212,12 @@ ALTER TABLE `quotes`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_buku`
+--
+ALTER TABLE `user_buku`
+  MODIFY `id_user_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
